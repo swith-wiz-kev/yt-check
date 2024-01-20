ab721c8daff9d2cd59c1577a8ad83d1b43298c6b
kev <ddrain10@gmail.com>
2024-01-20 12:47:13 +0800

    Add 6th batch of files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,14 @@ const path = require("path");
 const json5 = require("json5");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-let htmlPageNames = ["stayc1", "stayc2", "stayc3", "stayc4", "stayc5"];
+let htmlPageNames = [
+  "stayc1",
+  "stayc2",
+  "stayc3",
+  "stayc4",
+  "stayc5",
+  "stayc6",
+];
 let multipleHtmlPlugins = htmlPageNames.map((name) => {
   return new HtmlWebpackPlugin({
     title: `stayc fancams ${name.slice(-1)}`,
@@ -20,6 +27,7 @@ module.exports = {
     stayc3: "./src/stayc/stayc3.js",
     stayc4: "./src/stayc/stayc4.js",
     stayc5: "./src/stayc/stayc5.js",
+    stayc6: "./src/stayc/stayc6.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
